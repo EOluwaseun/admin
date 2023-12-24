37902d628135f5cd227647daeef17db997d5fd9e
Oluwaseun <99099642+EOluwaseun@users.noreply.github.com>
2023-12-24 10:24:58 -0800

    Update ViewEnq.js
    enquiry page modified

--- src/pages/ViewEnq.js
+++ src/pages/ViewEnq.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React,{useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useLocation, useNavigate } from 'react-router';
-import { getAEnquiry } from '../features/enquiry/enquirySlice';
-
-
-function ViewEnq() {
-  const location = useLocation()
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const getEnqId = location.pathname.split('/')[3]
-  const enqState = useSelector((state)=>state.enquiry)
-const {enqName,enqEmail,enMobile,enqComment, enqStatus} = enqState
-
-useEffect(()=>{
-  dispatch(getAEnquiry(getEnqId))
-},[dispatch, getEnqId])
-
-// go back
-const goBack =()=>{
-  navigate(-1)
-}
-return <div>ViewEnq</div>;
-=======
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
@@ -116,7 +91,6 @@ function ViewEnq() {
       </div>
     </div>
   );
->>>>>>> 7a6f35f748345dda2b9e2b96b94755679dfd50ea
 }
 
 export default ViewEnq;
